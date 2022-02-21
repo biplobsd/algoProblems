e1a0e4cb50b0beb7e69dfd1144edbd3707e0ca4b
Biplob Sutradhar <biplobsd11@gmail.com>
2022-02-22 02:01:38 +0600

    Add files via upload

---
+++ w5/prob-1/solution.c
@@ -0,0 +1,104 @@
+#include <stdio.h>
+
+
+void swap(int *x, int *y)
+{
+    int tamp;
+    tamp = *x;
+    *x = *y;
+    *y = tamp;
+}
+
+int partition(int array[], int left, int Right)
+{
+
+    int pivot = array[left];
+    while (left != Right)
+    {
+        if (pivot == array[left])
+        {
+            if (array[Right] < pivot)
+            {
+                Right = Right - 1;
+            }
+            else if (array[Right] >= pivot)
+            {
+                swap(&array[left], &array[Right]);
+                pivot = array[Right];
+                left = left + 1;
+            }
+        }
+        else if (pivot == array[Right])
+        {
+            if (array[left] > pivot)
+            {
+                left = left + 1;
+            }
+            else if (array[left] <= pivot)
+            {
+                swap(&array[left], &array[Right]);
+                pivot == array[left];
+                Right = Right - 1;
+            }
+        }
+    }
+    return left;
+}
+
+void Quicksort(int array[], int left, int Right)
+{
+    if (left < Right)
+    {
+        int pivot = partition(array, left, Right);
+        Quicksort(array, left, pivot - 1);
+        Quicksort(array, pivot + 1, Right);
+    }
+}
+
+void main()
+{
+    int n, arr[100], output[200], target, noteNeeded = 0, fillIn;
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    Quicksort(arr, 0, n-1);
+
+    scanf("%d", &target);
+
+    while (target > 0)
+    {
+        fillIn = 0;
+        for (int i = 0; i < n; i++)
+        {
+            if (target >= arr[i])
+            {
+                target -= arr[i];
+                output[noteNeeded]=arr[i];
+                noteNeeded++;
+                fillIn = 1;
+                break;
+            }
+        }
+        if (fillIn == 0)
+        {
+            break;
+        }
+    }
+    if (fillIn == 0)
+    {
+        printf("Impossible\n");
+    }
+    else
+    {
+        printf("%d\n", noteNeeded);
+        for (int i = 0; i < noteNeeded; i++)
+        {
+            printf("%d ", output[i]);
+        }
+    }
+}
+
+
